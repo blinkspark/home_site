41a61d3989c74bd40f487db0f30a5ff2a4bb049a
BlinkSpark <wangning2813@foxmail.com>
2018-11-07 17:57:58 +0800

    first blog

--- components/post_content.jsx
+++ components/post_content.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import Markdown from 'react-markdown'
+import React from "react"
+import Markdown from "react-markdown"
 
 class PostContent extends React.Component {
   render() {
-    let cNames = ['markdown']
+    let cNames = ["markdown"]
     cNames = cNames.concat(this.props.classToAdd)
     return (
-      <div className='markdown mb-5'>
-        <Markdown source={this.props.data}></Markdown>
+      <div className="markdown mb-5">
+        <hr />
+        <Markdown source={this.props.data} />
+        <hr />
       </div>
     )
   }
-
 }
 
 export default PostContent
