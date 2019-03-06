5620220e69e5960c478814ab23cb818f989d6f1e
Blinkspark <wangning2813@foxmail.com>
2019-03-06 17:15:34 +0800

    nuxt-starter

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]_[hash:base64:5]",
-    camelCase: true
-  }
-})
--- src/defines.js
+++
@@ -1,5 +0,0 @@
-const production = process.env.NODE_ENV === 'production'
-const HOST = production ? 'https://nealwang.top' : 'http://localhost:3000'
-module.exports = {
-  HOST
-}
