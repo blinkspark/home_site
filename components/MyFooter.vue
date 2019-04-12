1c6e1105f66c1dab035509380af5414244d125fb
Blinkspark <wangning2813@foxmail.com>
2019-04-12 12:09:36 +0800

    edit copyright

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-secondary py-3">
     <div class="container text-center text-light">
-      <div>Copyright 2019 Neal Wang All Rights Reserved.</div>
+      <div>Copyright © 2019 Neal Wang. All Rights Reserved.</div>
     </div>
   </footer>
 </template>
